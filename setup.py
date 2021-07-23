478378f28109df25d4e08233bf7c4b9f1bcb3032
maclandrol <emmanuel.noutahi@hotmail.ca>
2021-07-23 02:06:23 -0400

    bumped version to 1.1.0

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ MEDCHEM_PATH = os.path.join(BASEPATH, "medchem/lilly")
 
 # define project information
 NAME = "medchem"
-VERSION = "1.0.1"
+VERSION = "1.1.0"
 DESCRIPTION = "Molecule filtering code for medchem"
 
 

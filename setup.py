241b6a1eeb95ca34690d06674c8e5d1f310f9fb4
Hadrien Mary <hadrien.mary@gmail.com>
2021-08-24 16:37:11 -0400

    bumped version to 1.1.3

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ MEDCHEM_PATH = os.path.join(BASEPATH, "medchem/lilly")
 
 # define project information
 NAME = "medchem"
-VERSION = "1.1.2"
+VERSION = "1.1.3"
 DESCRIPTION = "Molecule filtering code for medchem"
 
 

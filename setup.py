568082ae9c64ab1b6dbbbaa66c26576b99f373f8
invivodev <invivodev@users.noreply.github.com>
2022-05-18 22:11:48 +0000

    bumped version to 1.3.2

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ MEDCHEM_PATH = os.path.join(BASEPATH, "medchem/lilly")
 
 # define project information
 NAME = "medchem"
-VERSION = "1.3.1"
+VERSION = "1.3.2"
 DESCRIPTION = "Molecule filtering code for medchem"
 
 
